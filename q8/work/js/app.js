241785c3a9407f59ed9211530db8b8ef746bd334
m-nkmr1205 <lucciola0322@gmail.com>
2022-12-26 21:40:39 +0900

    課題再提出3

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -15,16 +15,28 @@
 $(function(){
   let pageCount = 1;
    //グローバル変数として前回値を持たせる。検索ワードを前回と比較した時に変数の置き換え処理を行う。
-  let previousVal = $("#search-input").val("")
+  let previousVal = $("#search-input").val(null)
 
   // 検索ボタンをクリックした時に発火
   $(".search-btn").on("click", function(){
     const searchWord =  $("#search-input").val() //検索ワードを格納
+
+    // 検索ワードが同じ時と違う時の条件分岐
+    if(searchWord === previousVal){
+      pageCount += 1;
+    } else {
+      pageCount = 1;
+      previousVal = searchWord;  //今回の検索ワードをグローバル変数に置き換え
+      $('ul').empty()  //検索ワードが違う場合は前回の検索結果を削除する。
+    }
+
     const settings = {
       "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
       "method": "GET",
     }
     $(".message").remove();  //エラー・検索結果無しの時のメッセージがあれば削除する
+
+    
 
     //.doneが通信成功した時の処理、”response”が引数となっていて通信した結果を受け取っている
     $.ajax(settings).done(function (response) {
@@ -33,25 +45,13 @@ $(function(){
       //通信が成功した時の処理(検索結果があった場合、無かった場合のメッセージを表示)
       //引数にレスポンス結果を持たせる。
       displayResult(result)
-      console.log(response)
 
       //.failが通信に失敗した時の処理、”err”にサーバーから送られてきたエラー内容を受けとる。
       }).fail(function (err) {
         displayError(err)
       });
-      //ajaxの処理ここまで
-
-      // 検索ワードが同じ時と違う時の条件分岐
-      if(searchWord == previousVal){
-        pageCount += 1;
-        previousVal = searchWord;  //今回の検索ワードをグローバル変数に置き換え
-      } else {
-        pageCount = 1;
-        previousVal = searchWord;
-        $('ul').empty()  //検索ワードが違う場合は前回の検索結果を削除する。
-      }
+      //ajaxの処理ここまで      
     }); //検索ボタンのクリックイベントここまで
-
 
   // 検索結果を表示する関数。displayResultここから
   function displayResult(result) {
@@ -61,10 +61,10 @@ $(function(){
       // 書き方→for(初期値; 条件式; 増減式) {処理の実装}
       for (var i = 0; i < 20; i++){
       // resultから必要なデータを格納。 変数[i]を指定する事でresultの配列番号を指定
-        const title = result[i]['title'];
+        const title = result[i].title;
         const creator = result[i]['dc:creator'];
         const publisher = result[i]['dc:publisher'];
-        const info = result[i]['link']['@id'];
+        const info = result[i].link['@id'];
 
         // prepend()メソッド、指定の要素の先頭に子要素を追加
         // 検索結果を表示するHTML要素を追加
